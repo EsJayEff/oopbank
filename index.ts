f43d6141e4ee1d7612f97eb26c9ec8d91b7ebac2
Shiraz Javed Farooq <114878673+EsJayEff@users.noreply.github.com>
2022-12-12 01:06:16 +0500

    Simple Typescript and NodeJs OOP Bank App.

---
+++ index.ts
@@ -0,0 +1,134 @@
+#!usr/bin/env node
+
+import inquirer from "inquirer"
+
+class BankAccount implements IBankAccount
+{
+public AccountBalance:number
+public statement:string
+
+constructor(AccountBalance:number, statement:string)
+{
+    this.AccountBalance = AccountBalance;
+    this.statement= statement;
+}
+public BankAccount(n:number)
+{
+    this.AccountBalance=n;
+}
+public Debit (amount:number):string
+{
+this.statement="Sorry, you have insufficient balance!";
+if (amount>0)
+{
+this.statement= "The amount you have entered is incorrect";
+if (this.AccountBalance>amount)
+{
+this.AccountBalance=this.AccountBalance-amount;
+this.statement="Tranaction successful Your New Account Balance is: " + this.AccountBalance;
+}
+else
+{
+this.statement="You don't have enough money to do this tranaction: " + this.AccountBalance;    
+}
+}
+return this.statement;
+}
+
+public Credit (amount:number) :string
+{
+this.statement="Transaction Failed!";
+if (amount>0)
+{
+    this.AccountBalance=this.AccountBalance+amount;
+if(amount>100)
+{
+   this.AccountBalance=this.AccountBalance-1; 
+}
+this.statement="Your account has been credited successfully! Your New Account Balance is: " + this.AccountBalance;
+}
+return this.statement;
+}
+}
+
+
+class Customer extends BankAccount
+{
+public FirstName:string
+public LastName:string
+public Gender:string
+public Age:number
+public MobileNumber:string
+
+constructor(FirstName:string,LastName:string,Gender:string,Age:number,MobileNumber:string)
+{
+    super(0,"");
+    this.FirstName=FirstName;
+    this.LastName=LastName;
+    this.Gender=Gender;
+    this.Age=Age;
+    this.MobileNumber=MobileNumber;
+}
+public CustomerInfo()
+{
+return `${this.FirstName}, ${this.LastName}, ${this.Gender}, ${this.Age},${this.MobileNumber}`
+}
+}
+
+interface IBankAccount 
+{
+Debit (d:number):string
+Credit (d:number):string
+}
+
+const newUser = new Customer("Es","Jay","Male",42,"03343285085");
+newUser.statement= `Welcome ${newUser.FirstName} ${newUser.LastName}`
+newUser.BankAccount(100);
+
+async function askQuestion() {
+    const answers = await inquirer
+      .prompt([
+        /* Pass your questions in here */
+        {
+          type: "list",
+          name: "operator",
+          message: "\nWhich operation you want to perform\n",
+          choices: ["Debit", "Credit"],
+        },
+        {
+            type: "number",
+            name: "num1",
+            message: "Please Enter The Amount: ",
+        },
+      ]);
+      if (answers.operator == "Debit")
+{
+    newUser.Debit(answers.num1);
+    console.log(newUser.statement);
+    
+};
+
+      if (answers.operator == "Credit")
+{
+    newUser.Credit(answers.num1);
+    console.log(newUser.statement);
+};
+
+};
+
+async function startAgain() {
+    do {
+      await askQuestion();
+      var again = await inquirer.prompt({
+        type: "input",
+        name: "restart",
+        message: "Do you want to do more transactions Y/N: ",
+      });
+    } while (again.restart == "y" || again.restart == "Y");
+    console.log("Thank you for using the Esjay Bank! see you soon.");
+  }
+ 
+  startAgain();
+
+
+
